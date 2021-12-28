9dff1ac6b6248df6e153876178a1d82f547ffc40
Wawrzyniec Dobrucki <dobrucki@illinois.edu>
2021-12-28 14:39:57 -0600

    working version

--- inveon2slicer/inveon2slicer_gui.py
+++ inveon2slicer/inveon2slicer_gui.py
@@ -14,6 +14,7 @@
 #
 
 import sys
+import os
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
 from PyQt5 import QtGui as qtg
@@ -29,8 +30,8 @@ class MainWindow(qtw.QWidget):
         self.setWindowTitle(self.title)
         self.left = 100
         self.top = 100
-        self.width = 320
-        self.height = 240
+        self.width = 700
+        self.height = 250
         self.setGeometry(self.left, self.top, self.width, self.height)
 
         # Define widgets
@@ -38,8 +39,8 @@ class MainWindow(qtw.QWidget):
         self.load_button = qtw.QPushButton('LOAD')
 
         self.convert_label = qtw.QLabel('Save converted file as')
-        self.convert_button = qtw.QPushButton('CONVERT')
-        #convert_button.setFont(qtg.QFont('Bold', 13))
+        self.convert_button = qtw.QPushButton('CONVERT and SAVE')
+        self.convert_button.setEnabled(False)
 
         self.info_text = qtw.QTextEdit()
 
@@ -62,14 +63,79 @@ class MainWindow(qtw.QWidget):
 
     # Method: open dialog to select file to load
     def getfile(self):
-        fname, _ = qtw.QFileDialog.getOpenFileName(self,'Open Inveon header (*.hdr) file to convert', 'c:\\',"Header file (*.hdr)")
-        if fname:
-            print(fname)
+        self.fname, _ = qtw.QFileDialog.getOpenFileName(self,'Open Inveon header (*.hdr) file to convert', 'c:\\',"Header file (*.hdr)")
+        if self.fname:
+            self.convert_button.setEnabled(True)
+            return self.fname
 
     # Method: print info and convert file
     def convert_file(self):
-        self.info_text.setPlainText("PASS1\nPASS2")
-        print('PASS')
+        # read the *.img.hdr file
+        inveon_file = open(self.fname, "r")
+        # get filename only with no path
+        base = os.path.basename(self.fname)
+        # separate base from extension of a filename
+        base, extension = os.path.splitext(base)
+        # read lines from inveon *.hdr file for text analysis
+        lines = inveon_file.readlines()
+        # close the inveon *.hdr file
+        inveon_file.close()
+        
+        # information text to be displayed in a text window
+        info = 'File: ' + self.fname + '\n'
+
+        # first line of *.nhdr file
+        nhdr = [None] * 8  # initialize list
+        nhdr[0] = 'NRRD0001' + '\n'
+
+        # Text analysis: look for patterns such as filename, resolution, pixel dimension
+        for line in lines:
+            # find data type
+            if line.find('data_type') != -1:
+                data_type = line.split()
+                if data_type[1] == '2':
+                    nhdr[1] = 'type: int16' + '\n'  # 2-byte integer - Intel style
+                    info = info + 'Data type: \t2-byte integer (Intel style)\n'
+                else:
+                    nhdr[1] = 'type: float' + '\n'  # 4-byte float - Intel style
+                    info = info + 'Data type: \t4-byte float (Intel style)\n'
+
+            if line.find('x_dimension') != -1:
+                x_dim = line.split()
+                info = info + 'X dimension:\t' + x_dim[1] + ' px \n'
+            if line.find('y_dimension') != -1:
+                y_dim = line.split()
+                info = info + 'Y dimension:\t' + y_dim[1] + ' px \n'
+            if line.find('z_dimension') != -1:
+                z_dim = line.split()
+                info = info + 'Z dimension:\t' + z_dim[1] + ' px \n'
+            if line.find('pixel_size_x') != -1:
+                x_spacing = line.split()
+                info = info + 'X pixel size:\t' + x_spacing[1] + ' mm \n'
+            if line.find('pixel_size_y') != -1:
+                y_spacing = line.split()
+                info = info + 'Y pixel size:\t' + y_spacing[1] + ' mm \n'
+            if line.find('pixel_size_z') != -1:
+                z_spacing = line.split()
+                info = info + 'Z pixel size:\t' + z_spacing[1] + ' mm \n'
+
+        # print information text in info widget
+        self.info_text.setPlainText(info)
+
+        # fill nhdr list with info to create *.nhdr file
+        nhdr[2] = 'dimension: 3' + '\n'
+        nhdr[3] = 'sizes: ' + x_dim[1] + ' ' + y_dim[1] + ' ' + z_dim[1] + '\n'
+        nhdr[4] = 'spacings: ' + x_spacing[1] + ' ' + y_spacing[1] + ' ' + z_spacing[1] + '\n'
+        nhdr[5] = 'encoding: raw' + '\n'
+        nhdr[6] = 'data file: ' + base + '\n'
+        nhdr[7] = 'endian: little' + '\n' 
+
+        # save list as lines to the new file
+        save_file, _ = qtw.QFileDialog.getSaveFileName(self)
+        file = open(save_file + '.nhdr', "w+")
+        file.writelines(nhdr)
+        file.close()
+        
 
 # Start main loop and exit when quit signal
 if __name__ == '__main__':
